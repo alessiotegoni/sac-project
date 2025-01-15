9598ea8bdcd55d8d26b63c5334378fe555fee43c
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-15 15:59:57 +0100

    FIX layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import TripDetail from './pages/TripDetail';
 function App() {
     return (
         <BrowserRouter>
-            <Routes element={<DefaultLayout />}>
-                <Route index element={<HomePage />}></Route>
-                <Route path="trip/:id" element={<TripDetail />}></Route>
+            <Routes>
+                <Route element={<DefaultLayout />}>
+                    <Route index element={<HomePage />}></Route>
+                    <Route path="trip/:id" element={<TripDetail />}></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
     );
